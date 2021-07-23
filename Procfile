3193e18884d3663adacfd93d40559e1abbc9fcdd
Thomas <dosanjos.thomas@gmail.com>
2021-07-23 11:46:21 +0200

    quick update

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: SET NODE_ENV=production node src/app.js
+web: node src/app.js
--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const mongoDBAtlas = `mongodb+srv://admin:${process.env.MONGOATLAS_KEY}@task-app-db.kxhxu.mongodb.net/TaskAppDB?retryWrites=true&w=majority`;
 
 const urlDB =
-  process.env_NODE_ENV === "production"
-    ? mongoDBAtlas
-    : "mongodb://localhost:27017/task-app";
+  process.env_NODE_ENV === "development"
+    ? "mongodb://localhost:27017/task-app"
+    : mongoDBAtlas;
 
 mongoose
   .connect(urlDB, {
